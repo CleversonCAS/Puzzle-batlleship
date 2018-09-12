aa3a8cb81a6b51cd532995e474229882c68eb3a8
CleversonCAS <42379258+CleversonCAS@users.noreply.github.com>
2018-09-12 12:15:25 -0300

    Add files via upload

--- bp.cpp
+++ bp.cpp
@@ -5,6 +5,26 @@
 using std::cout;
 using std::cin;
 
+/*void proibido(COLOCAR MATRIZ AQUI)
+{
+	for(i = 0; i < n; ++i)
+	{
+		for(j = 0; j < m; ++j)
+		{
+			if(M[i][j]==1)
+			{
+				Mi[i][j+1] = Mi[i][j+1]==1 ? 1 : 2;
+				Mi[i][j-1] = Mi[i][j-1]==1 ? 1 : 2;
+				Mi[i+1][j] = Mi[i+1][j]==1 ? 1 : 2;
+				Mi[i-1][j] = Mi[i-1][j]==1 ? 1 : 2;
+				Mi[i+1][j+1] = Mi[i+1][j+1]==1 ? 1 : 2;
+				Mi[i-1][j-1] = Mi[i-1][j-1]==1 ? 1 : 2;
+				Mi[i-1][j+1] = Mi[i-1][j+1]==1 ? 1 : 2;
+				Mi[i+1][j-1] = Mi[i+1][j-1]==1 ? 1 : 2;
+			}
+		}
+	}
+}*/
 int main()
 {	
 
@@ -27,24 +47,16 @@ int main()
 	q = rand() % m ;
 	cout<<p<<q<<'\n';
 	*/
-	p=9;
-	q=1;
+	p=7;
+	q=8;
+	
+	
 	int hv;
 	hv = rand() % 2; // HV DE 0 E 1. SE HV = 0::HORIZONTAL | SE HV = 1::VERTICAL.
 	cout<<hv<<'\n';
 	if(board[p][q] == 0)
 	{
 		if(hv == 0)
-		{
-			if(board[p+1][q] == 0 && board[p+2][q] == 0 && board[p+3][q] == 0)
-			{
-				board[p][q] = 1;
-				board[p+1][q] = 1; 
-				board[p+2][q] = 1;
-				board[p+3][q] = 1;
-			}
-		}
-		if(hv == 1)
 		{
 			if(board[p][q+1] == 0 && board[p][q+2] == 0 && board[p][q+3] == 0)
 			{
@@ -54,7 +66,39 @@ int main()
 				board[p][q+3] = 1;
 			}
 		}
+		if(hv == 1)
+		{
+			if(board[p+1][q] == 0 && board[p+2][q] == 0 && board[p+3][q] == 0)
+			{
+				board[p][q] = 1;
+				board[p+1][q] = 1; 
+				board[p+2][q] = 1;
+				board[p+3][q] = 1;
+			}	
+		}
 	}
+	//WHILE (N DER CERTO)
+	//FAZER OS COISO, SE N DER CERTO , GERA OUTROS NUMEROS RANDOM
+
+
+	for(i = 0; i < n; ++i)
+	{
+		for(j = 0; j < m; ++j)
+		{
+			if(board[i][j]==1)
+			{
+				board[i][j+1] = board[i][j+1]==1 ? 1 : 2;
+				board[i][j-1] = board[i][j-1]==1 ? 1 : 2;
+				board[i+1][j] = board[i+1][j]==1 ? 1 : 2;
+				board[i-1][j] = board[i-1][j]==1 ? 1 : 2;
+				board[i+1][j+1] = board[i+1][j+1]==1 ? 1 : 2;
+				board[i-1][j-1] = board[i-1][j-1]==1 ? 1 : 2;
+				board[i-1][j+1] = board[i-1][j+1]==1 ? 1 : 2;
+				board[i+1][j-1] = board[i+1][j-1]==1 ? 1 : 2;
+			}
+		}
+	}
+
 	//print test
 	for(i = 0; i < n; ++i)
 	{
